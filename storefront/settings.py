7d78851503064a5ff0f413fb18349a4878928924
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-11-29 13:08:14 +0200

    Adding django debug sidebar

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
+    "store",
+    "tags",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +54,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'storefront.urls'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 TEMPLATES = [
     {
