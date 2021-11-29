7224989fd7433d03b1dca1c2fbcb96ddf2c00206
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-11-29 14:13:44 +0200

    Adding Product and custmer classes

--- store/models.py
+++ store/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=150, null=True, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=1)
+    weight = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    image = models.URLField(max_length=250, null=True, blank=True)
+    sku = models.CharField(max_length=15, null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True)
+    inventory = models.BooleanField(default=True)
+    tag = models.CharField(max_length=30, null=True, blank=True)
+    vendor = models.CharField(max_length=155, null=True, blank=True)
+
+
+class Customer(models.Model):
+    f_name = models.CharField(max_length=50, null=True, blank=True)
+    l_name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(
+        max_length=255, unique=True, null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True, unique=True)
+    create_date = models.DateField(auto_now_add=True)
