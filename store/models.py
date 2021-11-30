b5f0bb281f9e2e6bfabb5a2aedf48149c89e3322
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-11-30 12:33:47 +0200

    add meta class - for collection class to rename collection by title. - for collection class to order by title.

--- store/models.py
+++ store/models.py
@@ -11,6 +11,12 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, blank=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=150, null=True, blank=True)
