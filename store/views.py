865f1689adaf88e18f9c8c24afa8da69cc39bbea
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-12-01 17:09:51 +0200

    create review with product pk: - I overwrite the create method and get queryset method in the class - to grap the product id from the link in kwargs to create a new review. - the old attribute queryset= Review.objects.all() returns all views withot filtering. - and I have no access to self to apply filter by product id. - for that I overwrite it.

--- store/views.py
+++ store/views.py
@@ -36,5 +36,12 @@ class CollectionViewSet(ModelViewSet):
 
 
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
+    # this queryset didnt filter the reviews by id so we will overwrite it
+    # queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
