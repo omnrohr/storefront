5f2de45706aaa4e8959304fe2399e583ab324251
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-12-02 11:04:09 +0200

    add prefetch_related to minize the database queries when calling the data for cart

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.db.models.aggregates import Count
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.response import Response
-from rest_framework.mixins import CreateModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from django_filters.rest_framework import DjangoFilterBackend
@@ -58,6 +58,6 @@ class ReviewViewSet(ModelViewSet):
         return {'product_id': self.kwargs['product_pk']}
 
 
-class CartViewSet(CreateModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin, GenericViewSet, RetrieveModelMixin):
     serializer_class = CartSerializer
-    queryset = Cart.objects.all()
+    queryset = Cart.objects.prefetch_related('items__product').all()
